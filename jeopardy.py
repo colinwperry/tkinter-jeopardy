9ab2c8d069cae29b8b78cf4ca02b06cbdc1703fd
Colin Perry <colin.w.perry@proton.me>
2026-09-24 23:18:51 -0400

    feat: implemented timer logic and widget handling

--- jeopardy.py
+++ jeopardy.py
@@ -1,3 +1,4 @@
+import time
 import tkinter as tk
 
 from PIL import Image, ImageTk
@@ -17,68 +18,89 @@ class JeopardyGame:
             {
                 "question": "images/question_1.jpg",
                 "answer": "images/answer_1.jpg",
+                "is_complex": False,
             },
             {
                 "question": "images/question_2.jpg",
                 "answer": "images/answer_2.jpg",
+                "is_complex": False,
             },
             {
                 "question": "images/question_3.jpg",
                 "answer": "images/answer_3.jpg",
+                "is_complex": False,
             },
             {
                 "question": "images/question_4.jpg",
                 "answer": "images/answer_4.jpg",
+                "is_complex": False,
             },
             {
                 "question": "images/question_5.jpg",
                 "answer": "images/answer_5.jpg",
+                "is_complex": False,
             },
             {
                 "question": "images/question_6.jpg",
                 "answer": "images/answer_6.jpg",
+                "is_complex": False,
             },
             {
                 "question": "images/question_7.jpg",
                 "answer": "images/answer_7.jpg",
+                "is_complex": False,
             },
             {
                 "question": "images/question_8.jpg",
                 "answer": "images/answer_8.jpg",
+                "is_complex": False,
             },
             {
                 "question": "images/question_9.jpg",
                 "answer": "images/answer_9.jpg",
+                "is_complex": False,
             },
             {
                 "question": "images/question_10.jpg",
                 "answer": "images/answer_10.jpg",
+                "is_complex": False,
             },
             {
                 "question": "images/question_11.jpg",
                 "answer": "images/answer_11.jpg",
+                "is_complex": False,
             },
             {
                 "question": "images/question_12.jpg",
                 "answer": "images/answer_12.jpg",
+                "is_complex": False,
             },
             {
                 "question": "images/question_13.jpg",
                 "answer": "images/answer_13.jpg",
+                "is_complex": False,
             },
             {
                 "question": "images/question_14.jpg",
                 "answer": "images/answer_14.jpg",
+                "is_complex": False,
             },
             {
                 "question": "images/question_15.jpg",
                 "answer": "images/answer_15.jpg",
+                "is_complex": False,
             },
         ]
 
         self.create_board()
 
     def create_board(self):
+        timer_frame = tk.Frame(self.root, bg=self.FRAME_BG)
+        timer_frame.place(relx=0.5, rely=0.25, anchor="center")
+
+        self.timer_label = tk.Label(timer_frame, text="0:00", bg=self.WIDGET_BG, font=self.FONT_INFO)
+        self.timer_label.pack(padx=10, pady=10, ipadx=10, ipady=10)
+
         board_frame = tk.Frame(self.root, bg=self.FRAME_BG)
         board_frame.place(relx=0.5, rely=0.5, anchor="center")
 
@@ -112,6 +134,7 @@ class JeopardyGame:
                     "button": button,
                     "is_active": False,
                     "idx": idx,
+                    "complex": self.image_sets[idx]["is_complex"],
                 }
             )
 
@@ -128,13 +151,14 @@ class JeopardyGame:
         self.root.grid_columnconfigure(1, weight=1)
 
     def reveal_question(self, idx):
-        button_info = self.buttons[idx]
-        button = button_info["button"]
+        self.button_info = self.buttons[idx]
+        button = self.button_info["button"]
         image_set = self.image_sets[idx]
 
-        if button_info["is_active"] == False:
+        if self.button_info["is_active"] == False:
             question_image = Image.open(image_set["question"])
             question_image = question_image.resize((250, 200))
+            self.timer_length = 60 if self.button_info["complex"] == False else 120
 
             question_image_tk = ImageTk.PhotoImage(question_image)
 
@@ -144,10 +168,12 @@ class JeopardyGame:
             )
 
             button.image = question_image_tk
+            
+            self.button_info["is_active"] = True
+            self.start_time = time.time()
+            self.increment_timer()
 
-            button_info["is_active"] = True
-
-        elif button_info["is_active"] == True:
+        elif self.button_info["is_active"] == True:
             answer_image = Image.open(image_set["answer"])
             answer_image = answer_image.resize((250, 200))
 
@@ -160,7 +186,18 @@ class JeopardyGame:
 
             button.image = answer_image_tk
 
-            button_info["is_active"] = False
+            self.button_info["is_active"] = False
+
+    def increment_timer(self):
+        if self.button_info["is_active"]:
+            proper_time = int(self.start_time - time.time() + self.timer_length)
+            if proper_time <= 0:
+                self.label.configure(text="Times Up!")
+                return
+            root.after(1000, self.increment_timer)
+            minute, second = proper_time // 60, proper_time % 60
+            self.timer_label.configure(text=f"{minute}:{second:02}")
+
 
 
 if __name__ == "__main__":
